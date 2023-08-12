e8d9889db6359e08cea78418d12c388c383a6b1c
JainPriya1234 <priya.jain11302@gmail.com>
2023-08-12 15:30:51 +0530

    bug fix

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -96,7 +96,7 @@ const signin = async (req,res,next)=>{
             token: generateJWT(emailExists),
             role:emailExists.role
         };
-        res.status(200).json("successfully logged in ! ");
+        res.status(200).json(next(sendSuccessApiResponse(data)));
       }
     catch(err){
         res.json(err);
